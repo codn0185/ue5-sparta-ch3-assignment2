3e9555082c63188c87b46182df9189e4c006a6db
김채우 <codn0185@gmail.com>
2026-08-17 09:57:10 +0900

    feat: 게임 모드 추가 및 Pawn/Controller 설정

---
+++ Source/SpartaCH3Assignment2/Public/MyGameMode.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/GameMode.h"
+
+#include "MyGameMode.generated.h"
+
+UCLASS()
+class SPARTACH3ASSIGNMENT2_API AMyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+  public:
+	AMyGameMode();
+};
